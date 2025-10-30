a0c2935b4e0e98eedb18cd741ae6f483dfdf3851
KANIJ SULTANA <KANIJ SULTANA@DESKTOP-30C944H>
2025-10-30 22:33:25 +0600

    Add project files.

---
+++ Class_02/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            //variables
+            string Name = "kanij";
+         
+
+            string city = "Dhaka";
+            string favouritFood = "Alu vorta";
+            int birthYear = 1998;
+            int currentYear = 2025;
+            int age = currentYear - birthYear;
+
+            Console.WriteLine("And my age current is" +   );
+            Console.WriteLine("My Name is" + Name);
+            Console.WriteLine("i live in : +city");
+            Console.WriteLine("my favourite food is :" + favouritFood);
+            Console.WriteLine("My Age is:");
+
+            Console.ReadKey();
+            
+        }
+    }
+}
